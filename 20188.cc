17e67b25d2d6737fb9c6060177e56603c39fed8d
wxogus25 <59160609+wxogus25@users.noreply.github.com>
2022-03-20 17:17:12 +0900

    [Platinum V]   20188  등산 마니아

--- 20188.cc
+++ 20188.cc
@@ -1,4 +1,4 @@
-// 2022-03-12 22:23:31
+// 2022-03-12 22:45:59
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -11,8 +11,6 @@ void func(int now, int parent, long long level)
 {
 	if (trsize[now])
 		return;
-	trsize[now] = 1;
-
 	int len = edge[now].size();
 	for (int i = 0; i < len; i++)
 	{
@@ -23,17 +21,15 @@ void func(int now, int parent, long long level)
 		edcnt[now] += edcnt[edge[now][i]] + trsize[edge[now][i]];
 		ans += edcnt[edge[now][i]] + trsize[edge[now][i]] + trsize[edge[now][i]] * level;
 	}
+	long long ck = trsize[now];
 	for (int i = 0; i < len; i++)
 	{
 		if (edge[now][i] == parent)
 			continue;
-		for (int j = i + 1; j < len; j++)
-		{
-			if (edge[now][j] == parent)
-				continue;
-			ans += (trsize[edge[now][i]] + edcnt[edge[now][i]]) * trsize[edge[now][j]] + (trsize[edge[now][j]] + edcnt[edge[now][j]]) * trsize[edge[now][i]] + trsize[edge[now][i]] * trsize[edge[now][j]] * level;
-		}
+		ans += (trsize[edge[now][i]] + edcnt[edge[now][i]]) * (trsize[now] - trsize[edge[now][i]]) + (ck - trsize[edge[now][i]]) * trsize[edge[now][i]] * level;
+		ck -= trsize[edge[now][i]];
 	}
+	trsize[now]++;
 }
 
 int main()
