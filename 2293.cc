79ce65f63e9c15f8b5e195d679a27ee8d5e999a2
wxogus25 <59160609+wxogus25@users.noreply.github.com>
2022-03-20 17:17:06 +0900

    [Gold V]       2293   동전 1

---
+++ 2293.cc
@@ -0,0 +1,19 @@
+// 2022-03-07 13:04:10
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, dp[10010]={0,}, k, d;
+    cin >> n >> k;
+    dp[0]=1;
+    while(n--){
+        cin >> d;
+        for(int j=d;j<=k;j++){
+            dp[j] += dp[j-d];
+        }
+    }
+    cout << dp[k];
+}
