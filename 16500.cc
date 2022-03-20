375e15e79e3d1a7aaf99fdf279274c30c51ccb9b
wxogus25 <59160609+wxogus25@users.noreply.github.com>
2022-03-20 17:16:42 +0900

    [Silver I]     16500  문자열 판별

---
+++ 16500.cc
@@ -0,0 +1,57 @@
+// 2022-02-17 22:45:35
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <functional>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+int n, len, ls[110], dp[110];
+char s[110], d[110][110];
+
+bool func(int now){
+    if(now == len){
+        return true;
+    }
+    int &res = dp[now];
+    if(res != -1){
+        return res;
+    }
+    res = false;
+    char temp;
+    int slen;
+    for(int i=0;i<n;i++){
+        slen = ls[i];
+        if(len-now < slen){
+            continue;
+        }
+        temp = s[now+slen];
+        s[now+slen] = '\0';
+        if(strcmp(d[i],s+now) == 0){
+            s[now+slen]=temp;
+            if(func(now+slen)){
+                return res = true;
+            }
+        }
+        s[now+slen]=temp;
+    }
+    return res;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> s;
+    len = strlen(s);
+    fill(dp,dp+len,-1);
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> d[i];
+        ls[i] = strlen(d[i]);
+    }
+    cout << func(0) << endl;
+}
