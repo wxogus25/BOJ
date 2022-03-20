11580862d69b4eb82c8a8b567bed277bea99cd47
wxogus25 <59160609+wxogus25@users.noreply.github.com>
2022-03-20 17:36:16 +0900

    [Gold V]       11758  CCW

---
+++ 11000~11999/11758.cc
@@ -0,0 +1,22 @@
+// 2022-03-06 22:32:41
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cross(int x1, int y1, int x2, int y2){
+    return x1*y2 - x2*y1;
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int x1, x2, x3, y1, y2, y3, t;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    t = cross(x2 - x1, y2 - y1, x3 - x2, y3 - y2);
+    if(t < 0){
+        cout << -1;
+    }else if(t > 0){
+        cout << 1;
+    }else{
+        cout << 0;
+    }
+}
