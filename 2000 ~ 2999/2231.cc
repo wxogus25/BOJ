18a21c62ef38e2c953658030be9d8470b62e145d
wxogus25 <59160609+wxogus25@users.noreply.github.com>
2022-03-25 15:37:28 +0900

    [Bronze II]    2231   분해합

---
+++ 2000 ~ 2999/2231.cc
@@ -0,0 +1,29 @@
+// 2022-02-15 00:06:10
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int s = i, a = i;
+        while(a){
+            s += a%10;
+            a/=10;
+        }
+        if(s == n){
+            cout << i;
+            return 0;
+        }
+    }
+    cout << 0;
+}
